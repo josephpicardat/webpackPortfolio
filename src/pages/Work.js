37ceb001c00b7543a36f9f2368a2303d6b232c08
Joseph <josephpicardat1@gmail.com>
2024-05-12 21:34:43 -0500

    updated file config due to case sensitivity

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Projects from '../components/work/Projects.js';
+import Projects from '../components/Work/Projects.js';
 import '../components/css/work/work.css';
 
 const Work = () => {
